8f1039952937a0f07b0a9941b900ff6cf5e62209
Hank <853160447@qq.com>
2019-08-05 22:12:26 +0800

    server

--- etcd_test.go
+++ etcd_test.go
@@ -6,12 +6,36 @@ import (
 	"testing"
 )
 
-func TestEtcdMain(t *testing.T){
+func TestInfraOEtcdMain(t *testing.T){
 	os.Args = []string{"etcd-3.3.12-test","--name","infraO",
 		"--initial-advertise-peer-urls","http://127.0.0.1:2380",
 		"--listen-peer-urls","http://127.0.0.1:2380",
 		"--listen-client-urls","http://127.0.0.1:2379",
 		"--advertise-client-urls","http://127.0.0.1:2379",
+		"--initial-cluster-token","etcd-cluster-1",
+		"--initial-cluster","infraO=http://127.0.0.1:2380,infral=http://127.0.0.1:2382,infra2=http://127.0.0.1:2384",
+		"--initial-cluster-state","new"}
+	etcdmain.Main()//服务端主入口
+}
+
+func TestInfra1EtcdMain(t *testing.T){
+	os.Args = []string{"etcd-3.3.12-test","--name","infra1",
+		"--initial-advertise-peer-urls","http://127.0.0.1:2382",
+		"--listen-peer-urls","http://127.0.0.1:2382",
+		"--listen-client-urls","http://127.0.0.1:2381",
+		"--advertise-client-urls","http://127.0.0.1:2381",
+		"--initial-cluster-token","etcd-cluster-1",
+		"--initial-cluster","infraO=http://127.0.0.1:2380,infral=http://127.0.0.1:2382,infra2=http://127.0.0.1:2384",
+		"--initial-cluster-state","new"}
+	etcdmain.Main()//服务端主入口
+}
+
+func TestInfra2EtcdMain(t *testing.T){
+	os.Args = []string{"etcd-3.3.12-test","--name","infra2",
+		"--initial-advertise-peer-urls","http://127.0.0.1:2384",
+		"--listen-peer-urls","http://127.0.0.1:2384",
+		"--listen-client-urls","http://127.0.0.1:2383",
+		"--advertise-client-urls","http://127.0.0.1:2383",
 		"--initial-cluster-token","etcd-cluster-1",
 		"--initial-cluster","infraO=http://127.0.0.1:2380,infral=http://127.0.0.1:2382,infra2=http://127.0.0.1:2384",
 		"--initial-cluster-state","new"}
