76102bcfea984f7a6630a755f327e3292daba298
Hank <853160447@qq.com>
2020-10-26 16:13:01 +0800

    stream

--- main/etcd_test.go
+++ main/etcd_test.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"crypto/x509"
 	"github.com/friendlyhank/etcd-3.4-annotated/etcdmain"
+	"github.com/friendlyhank/etcd-3.4-annotated/pkg/transport"
+	"io/ioutil"
 	"os"
+	"runtime"
+	"strings"
 	"testing"
 )
+
+//生成https签名校验
+func createSelfCert(host string) (*transport.TLSInfo, func(), error) {
+	return createSelfCertEx(host)
+}
+
+func createSelfCertEx(host string, additionalUsages ...x509.ExtKeyUsage) (*transport.TLSInfo, func(), error) {
+	d, terr := ioutil.TempDir("", "etcd-test-tls-")
+	if terr != nil {
+		return nil, nil, terr
+	}
+	info, err := transport.SelfCert(nil, d, []string{host}, additionalUsages...)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &info, func() { os.RemoveAll(d) }, nil
+}
 
 //生产环境中端口应该是一样的，IP不同
 //启动测试服务
@@ -52,6 +74,45 @@ func TestInfra2EtcdMain(t *testing.T){
 		"--initial-cluster-token","etcd-cluster-1",
 		"--initial-cluster","infra0=http://127.0.0.1:2380,infra1=http://127.0.0.1:2382,infra2=http://127.0.0.1:2384",
 		"--initial-cluster-state","new"}
+	StartInfraServer(os.Args)
+}
+
+func TestInfraOEtcdMainWithTLS(t *testing.T){
+	peerTls, peerDel, peerErr := createSelfCert("127.0.0.1:2380")
+	clientTls, clientDel, clientErr := createSelfCert("127.0.0.1:2379")
+	if peerErr != nil {
+		t.Fatalf("unable to create peercert: %v", peerErr)
+	}
+	if clientErr != nil {
+		t.Fatalf("unable to create clientcert: %v", clientErr)
+	}
+	defer peerDel()
+	defer clientDel()
+
+	if runtime.GOOS == "windows" {
+		clientTls.CertFile = strings.ReplaceAll(clientTls.CertFile, "\\", "/")
+		clientTls.KeyFile = strings.ReplaceAll(clientTls.KeyFile, "\\", "/")
+		peerTls.CertFile = strings.ReplaceAll(peerTls.CertFile, "\\", "/")
+		peerTls.KeyFile = strings.ReplaceAll(peerTls.KeyFile, "\\", "/")
+	}
+
+	os.Args = []string{"etcd-3.3.12-test", "--name", "infra0",
+		"--initial-advertise-peer-urls", "http://127.0.0.1:2380",
+		"--listen-peer-urls", "http://127.0.0.1:2380",
+		"--listen-client-urls", "http://127.0.0.1:2379",
+		"--advertise-client-urls", "http://127.0.0.1:2379",
+		"--initial-cluster-token", "etcd-cluster-1",
+		"--initial-cluster", "infra0=http://127.0.0.1:2380,infra1=http://127.0.0.1:2382,infra2=http://127.0.0.1:2384",
+		"--initial-cluster-state", "new",
+		//"trusted-ca-file" //受信任的证书办法机构
+		//"peer-trusted-ca-file" //perr受信任的证书办法机构
+		"--cert-file", clientTls.CertFile,
+		"--key-file", clientTls.KeyFile,
+		"--peer-cert-file", peerTls.CertFile,
+		"--peer-key-file", peerTls.KeyFile,
+		"--peer-client-cert-auth=true", //peer cert校验
+		"--client-cert-auth=true",      //客户端cert校验
+	}
 	StartInfraServer(os.Args)
 }
 
